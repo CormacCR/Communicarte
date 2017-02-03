c92de43f0e5d498bfb6f41b28319803234b91cb6
Cormac Clancy Ruiz <clancyrc@tcd.ie>
2017-02-03 12:16:04 +0000

    Initial commit

---
+++ app/src/main/java/com/example/cormacclancyruiz/communicarte/JoinSession.java
@@ -0,0 +1,13 @@
+package com.example.cormacclancyruiz.communicarte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_session);
+    }
+}
---
+++ app/src/main/java/com/example/cormacclancyruiz/communicarte/MainMenu.java
@@ -0,0 +1,39 @@
+package com.example.cormacclancyruiz.communicarte;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+
+
+public class MainMenu extends AppCompatActivity {
+
+    Button createSession;
+    Button joinSession;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        createSession = (Button) findViewById(R.id.create_session);
+        joinSession = (Button) findViewById(R.id.join_session);
+
+    }
+
+    protected void createSession (View view){
+        Intent createSessionPage = new Intent(this,CreateSession.class);
+        startActivity(createSessionPage);
+    }
+
+    protected void joinSession (View view){
+        Intent joinSessionPage = new Intent(this,JoinSession.class);
+        startActivity(joinSessionPage);
+    }
+
+
+}
